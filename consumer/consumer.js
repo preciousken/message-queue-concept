53776b43d9837a8d21440c41bc72cd97f6ddba91
preciousken <pkehinde27@gmail.com>
2023-11-12 06:31:17 +0100

    updated

--- consumer/consumer.js
+++ consumer/consumer.js
@@ -14,65 +14,26 @@ app.listen(port,()=>{
   console.log('app listening on port '+port+'...')
 })
 
-app.post("/consume-queue",async (req,res)=>{
-try {
-  let body = req.body;
 
-  // checking if right data provided
-  if(!body.queueId){
-    return req.status(400).json({
-      status:false,
-      message:"input your queueId"
-    })
-  }
-  if(!body.data){
-    return res.status(400).json({
-      status:false,
-      message:"input your data"
-    })
-  }
+async function emailConsumer() {
+  const connection = await amqp.connect('amqp://localhost');
+  const channel = await connection.createChannel();
+  const queue = 'task_queue';
 
-  // send a response then proceed with your quues
-  res.status(200).json({
-    status:true,
-    message:"Data queued successfully",
-  })
+  await channel.assertQueue(queue, { durable: true });
+  channel.prefetch(1);
 
-  // queue Emails
-  if(body.queueId === "sendemail"){
-    await queueEmail(body.queueId,body.data)
-  }
+  console.log(`Waiting for messages. To exit, press CTRL+C`);
 
+  channel.consume(queue, (msg) => {
+    const message = msg.content.toString();
+    console.log(`Received message: ${message}`);
 
-
-} catch (error) {
-  console.log(error)
-  return req.status(500).json({
-    status:false,
-    message: "something completely went wrong"
-  })
+    // Simulate task processing
+    setTimeout(() => {
+      console.log(`Task completed: ${message}`);
+      channel.ack(msg);
+    }, 1000);
+  });
 }
-})
-
-// async function emailConsumer() {
-//   const connection = await amqp.connect('amqp://localhost');
-//   const channel = await connection.createChannel();
-//   const queue = 'task_queue';
-
-//   await channel.assertQueue(queue, { durable: true });
-//   channel.prefetch(1);
-
-//   console.log(`Waiting for messages. To exit, press CTRL+C`);
-
-//   channel.consume(queue, (msg) => {
-//     const message = msg.content.toString();
-//     console.log(`Received message: ${message}`);
-
-//     // Simulate task processing
-//     setTimeout(() => {
-//       console.log(`Task completed: ${message}`);
-//       channel.ack(msg);
-//     }, 1000);
-//   });
-// }
-// emailConsumer()
+emailConsumer()
--- producer/controller/email.js
+++ producer/controller/email.js
@@ -4,20 +4,30 @@ const amqp = require('amqplib');
 const sendEmail = async (req,res,next) =>{
 try {
 
-    const connection = await amqp.connect('amqp://localhost');
-    const channel = await connection.createChannel();
-    const queue = 'task_queue';
-  
-    await channel.assertQueue(queue, { durable: true });
-  
-    const message = 'Hello, RabbitMQ!';
-    channel.sendToQueue(queue, Buffer.from(message), { persistent: true });
-  
-    console.log(`Message sent: ${message}`);
-  
-    setTimeout(() => {
-      connection.close();
-    }, 500);
+    let body = req.body;
+    // getting the message data
+    if(!body.queueId){
+        return res.status(400).json({
+            status:false,
+            message:"please input your queue Id",
+        })
+    }
+    if(!body.data){
+        return res.status(400).json({
+            status:false,
+            message:"please input your message data",
+        })
+    }
+
+
+    // Connect to RabbitMQ
+  const connection = await amqp.connect('amqp://localhost');
+  const channel = await connection.createChannel();
+
+  // declare a queue
+  await channel.assertQueue(body.queueId);
+  // Send message to the queue
+  channel.sendToQueue(body.queueId, Buffer.from(body.data));
 
     return res.status(200).json({
         status:true,
