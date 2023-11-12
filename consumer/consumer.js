92c866c85d19ee2d413652cd02188040114b5f4f
preciousken <pkehinde27@gmail.com>
2023-11-12 05:55:58 +0100

    updated

--- consumer/consumer.js
+++ consumer/consumer.js
@@ -13,23 +13,29 @@ app.listen(port,()=>{
   console.log('app listening on port '+port+'...')
 })
 
-app.post("/consume-queue",async = (req,res)=>{
+app.post("/consume-queue",async (req,res)=>{
 try {
   let body = req.body;
 
-  
+  // checking if right data provided
   if(!body.queueId){
-    return req.status(500).json({
+    return req.status(400).json({
       status:false,
       message:"input your queueId"
     })
   }
-} catch (error) {
-  console.log(error)
-}
-})
+  if(!body.data){
+    return res.status(400).json({
+      status:false,
+      message:"input your data"
+    })
+  }
 
-async function emailConsumer() {
+  res.status(200).json({
+    status:true,
+    message:"Data queued successfully",
+  })
+
   const connection = await amqp.connect('amqp://localhost');
   const channel = await connection.createChannel();
   const queue = 'task_queue';
@@ -49,5 +55,35 @@ async function emailConsumer() {
       channel.ack(msg);
     }, 1000);
   });
+
+} catch (error) {
+  console.log(error)
+  return req.status(500).json({
+    status:false,
+    message: "something completely went wrong"
+  })
 }
-emailConsumer()
+})
+
+// async function emailConsumer() {
+//   const connection = await amqp.connect('amqp://localhost');
+//   const channel = await connection.createChannel();
+//   const queue = 'task_queue';
+
+//   await channel.assertQueue(queue, { durable: true });
+//   channel.prefetch(1);
+
+//   console.log(`Waiting for messages. To exit, press CTRL+C`);
+
+//   channel.consume(queue, (msg) => {
+//     const message = msg.content.toString();
+//     console.log(`Received message: ${message}`);
+
+//     // Simulate task processing
+//     setTimeout(() => {
+//       console.log(`Task completed: ${message}`);
+//       channel.ack(msg);
+//     }, 1000);
+//   });
+// }
+// emailConsumer()
